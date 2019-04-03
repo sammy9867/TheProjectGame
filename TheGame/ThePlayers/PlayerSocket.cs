4e6bc52cf41949f0379047e75f61ccf874f21067
Mike Urmich <urmich.m@gmail.com>
2019-04-04 01:40:03 +0200

    Servers classes added, not working yet

--- TheGame/ThePlayers/PlayerSocket.cs
+++ TheGame/ThePlayers/PlayerSocket.cs
@@ -11,38 +11,155 @@ namespace ThePlayers
 {
     class PlayerSocket
     {
-        private int PORT;
-        private IPAddress ipAddress;
+        private const int port = 11000;
 
-        public Socket socket;
+        // ManualResetEvent instances signal completion.
+        private static ManualResetEvent connectDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent sendDone =
+            new ManualResetEvent(false);
+        private static ManualResetEvent receiveDone =
+            new ManualResetEvent(false);
 
-        PlayerSocket(int port, IPAddress ip)
+        private static String response = String.Empty;
+
+        public static Socket server;
+
+        public static void StartClient()
         {
-            this.PORT = port;
-            this.ipAddress = ip;
+            try
+            {
+                IPAddress ipAddress = IPAddress.Loopback;
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
 
-            socket = new Socket(AddressFamily.InterNetwork,
-                SocketType.Stream,
-                ProtocolType.Tcp);
+                server = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp);
+                server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendTimeout, 1000);
+                server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Linger, new LingerOption(true, 10));
 
-            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.SendTimeout, 1000);
-            socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Linger, new LingerOption(true, 10));
+                server.BeginConnect(remoteEP,
+                    new AsyncCallback(ConnectCallback), server);
+                connectDone.WaitOne();
 
-            socket.BeginConnect(new IPEndPoint(ipAddress, PORT),
-                new AsyncCallback(ConnectCallback), socket);
-            
-        }
+                //Send(xml.ToString() + (char)23);
+                //sendDone.WaitOne();
 
-        private void ConnectCallback(IAsyncResult ar)
-        {
-            try {
-                Socket client = (Socket)ar.AsyncState;
-                client.EndConnect(ar);
-                Console.WriteLine("Socket connected");
-            }catch(Exception e)
+                // Receive the response from the remote device.
+                //Receive();
+                //receiveDone.WaitOne();
+
+                // Release the socket.
+                //client.Shutdown(SocketShutdown.Both);
+                //client.Close();
+
+            }
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
         }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+                client.EndConnect(ar);
+
+                Console.WriteLine("Socket connected to {0}",
+                    client.RemoteEndPoint.ToString());
+
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void Receive(Action<string> cb = null)
+        {
+            try
+            {
+                StateObject state = new StateObject();
+                state.cb = cb;
+
+                server.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), state);
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                StateObject state = (StateObject)ar.AsyncState;
+
+                int bytesRead = server.EndReceive(ar);
+                if (bytesRead > 0)
+                {
+                    state.sb = state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                    if (state.sb.ToString().IndexOf((char)23) < 0)
+                    {
+                        server.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                            new AsyncCallback(ReceiveCallback), state);
+                        return;
+                    }
+                }
+                var content = state.sb.ToString();
+                content = content.Remove(content.IndexOf((char)23), 1);
+                Console.WriteLine("Read {0} bytes from socket. \nData : {1}",
+                    content.Length, content);
+
+                if (state.cb != null)
+                {
+                    state.cb(content);
+                    state.cb = null;
+                    receiveDone.Set();
+                }
+                else
+                {
+//                    RequestHandler.handleRequest(content, server);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void Send(Socket handler, String data, Action<string> cb = null)
+        {
+            //cb = _cb;
+            byte[] byteData = Encoding.ASCII.GetBytes(data + (char)23);
+            handler.BeginSend(byteData, 0, byteData.Length, 0,
+                new AsyncCallback(SendCallback), handler);
+
+            Receive(cb);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Socket client = (Socket)ar.AsyncState;
+
+                int bytesSent = client.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
+
+                sendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
     }
 }
