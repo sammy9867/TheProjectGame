3bd6bbf94ad731ba1fb4a52c4206068948912fe7
Yuliia Ivashchenko <ivashchenkoy@student.mini.pw.edu.pl>
2019-06-03 21:54:26 +0200

    [IntegrationTests] Checking for color in a scenario where only blue players play

--- TheGame/TheGame.IntegrationTests/UnitTest1.cs
+++ TheGame/TheGame.IntegrationTests/UnitTest1.cs
@@ -43,8 +43,21 @@ namespace TheGame.IntegrationTests
             string val1 = cellValue1.ToString();
             string ex = "Connect";
 
+            string ex_bl = "blue";
+
+            int c = dt.Rows.Count;
+            for (int i = 0; i < c - 1; i++)
+            {
+                Object cellValue_blue = dt.Rows[i][3];
+                string val_blue = cellValue_blue.ToString();
+                Assert.Contains(ex_bl, val_blue);
+            }
+            
+
+
             Assert.Contains(ex, val0);
             Assert.Contains(ex, val1);
+            
             //double otherNumber = dt.Rows[1].Field<string>("Type");
 
         }
